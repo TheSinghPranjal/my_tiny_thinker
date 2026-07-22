df5b668ed9bcd126f3e2d6ba7b5ed03170d260d2
TheSinghPranjal <pranjalsingh10061997@gmail.com>
2026-07-22 09:55:33 +0530

    commit : premium fixes

--- lib/games/color_balloon_pop/presentation/color_balloon_pop_game_screen.dart
+++ lib/games/color_balloon_pop/presentation/color_balloon_pop_game_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_tiny_thinker/core/models/reward_model.dart';
+import 'package:my_tiny_thinker/core/routing/game_navigation.dart';
 import 'package:my_tiny_thinker/core/constants/app_spacing.dart';
 import 'package:my_tiny_thinker/core/widgets/game_session_hud.dart';
 import 'package:my_tiny_thinker/core/providers/settings_provider.dart';
@@ -51,7 +53,15 @@ class _ColorBalloonPopGameScreenState
     });
   }
 
-  void _start() {
+  Future<void> _start() async {
+    if (!await ensureCanStartGame(
+      context,
+      ref,
+      GameId.colorBalloonPop,
+    )) {
+      return;
+    }
+    if (!mounted) return;
     _saved = false;
     final settings = ref.read(colorBalloonPopSettingsProvider);
     ref.read(colorBalloonPopControllerProvider.notifier).reset();
