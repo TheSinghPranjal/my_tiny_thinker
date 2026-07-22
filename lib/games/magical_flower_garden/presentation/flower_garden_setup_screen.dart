934a85bd5271d33852483724152aabf9df96306b
TheSinghPranjal <pranjalsingh10061997@gmail.com>
2026-07-22 09:56:15 +0530

    commit : premium fixes

--- lib/games/magical_flower_garden/presentation/flower_garden_setup_screen.dart
+++ lib/games/magical_flower_garden/presentation/flower_garden_setup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+import 'package:my_tiny_thinker/core/models/reward_model.dart';
+import 'package:my_tiny_thinker/core/routing/game_navigation.dart';
 import 'package:my_tiny_thinker/core/routing/app_router.dart';
 import 'package:my_tiny_thinker/core/widgets/game_setup_scaffold.dart';
 import 'package:my_tiny_thinker/games/magical_flower_garden/models/flower_garden_models.dart';
@@ -27,7 +28,7 @@ class FlowerGardenSetupScreen extends ConsumerWidget {
           titleShadows: const [
                       Shadow(color: Color(0xFF2E7D32), blurRadius: 6),
                     ],
-          onPlay: () => context.push(AppRoutes.flowerGardenGame),
+          onPlay: () => pushGameGuarded(context, ref, GameId.magicalFlowerGarden, AppRoutes.flowerGardenGame),
         ),
       ),
     );
