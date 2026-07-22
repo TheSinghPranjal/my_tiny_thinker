58e34b03caa3f4f037f8cba4242371caa9e7917c
TheSinghPranjal <pranjalsingh10061997@gmail.com>
2026-07-22 09:56:10 +0530

    commit : premium fixes

--- lib/games/magical_flower_garden/presentation/flower_garden_game_screen.dart
+++ lib/games/magical_flower_garden/presentation/flower_garden_game_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_tiny_thinker/core/models/reward_model.dart';
+import 'package:my_tiny_thinker/core/routing/game_navigation.dart';
 import 'package:my_tiny_thinker/core/routing/app_router.dart';
 import 'package:my_tiny_thinker/core/widgets/game_session_hud.dart';
 import 'package:my_tiny_thinker/core/services/audio_service.dart';
@@ -48,7 +50,15 @@ class _FlowerGardenGameScreenState extends ConsumerState<FlowerGardenGameScreen>
     });
   }
 
-  void _start() {
+  Future<void> _start() async {
+    if (!await ensureCanStartGame(
+      context,
+      ref,
+      GameId.magicalFlowerGarden,
+    )) {
+      return;
+    }
+    if (!mounted) return;
     _saved = false;
     final settings = ref.read(flowerGardenSettingsProvider);
     ref.read(flowerGardenControllerProvider.notifier).reset();
