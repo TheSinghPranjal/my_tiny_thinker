1bcf27eea0ebd03e8db85d6176830b00c8f0c0b5
TheSinghPranjal <pranjalsingh10061997@gmail.com>
2026-08-14 14:51:42 +0530

    commit : animal sound hud and bunny hop candy color fixes

--- lib/games/catch_the_fish/presentation/catch_the_fish_setup_screen.dart
+++ lib/games/catch_the_fish/presentation/catch_the_fish_setup_screen.dart
@@ -6,15 +6,13 @@ import 'package:my_tiny_thinker/core/routing/app_router.dart';
 import 'package:my_tiny_thinker/core/theme/colors/app_colors.dart';
 import 'package:my_tiny_thinker/core/widgets/game_setup_scaffold.dart';
 import 'package:my_tiny_thinker/games/catch_the_fish/models/catch_the_fish_models.dart';
-import 'package:my_tiny_thinker/games/catch_the_fish/presentation/widgets/ocean_fishing_background.dart';
 
 class CatchTheFishSetupScreen extends ConsumerWidget {
   const CatchTheFishSetupScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return OceanFishingBackground(
-      child: Scaffold(
+    return Scaffold(
         backgroundColor: Colors.transparent,
         body: GameSetupScaffold(
           emoji: '🎣🐠',
@@ -30,7 +28,6 @@ class CatchTheFishSetupScreen extends ConsumerWidget {
           ],
           onPlay: () => pushGameGuarded(context, ref, GameId.catchTheFishAdventure, AppRoutes.catchTheFishGame),
         ),
-      ),
-    );
+      );
   }
 }
